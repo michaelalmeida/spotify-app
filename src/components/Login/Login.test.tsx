1549d16b66dfb5b7ec4d821b46f6f924fe3e1cc2
Michael Almeida <michaelralmeida@gmail.com>
2020-08-31 23:01:36 -0300

    Initial test setup + initial Login tests

---
+++ src/components/Login/Login.test.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { cleanup } from "@testing-library/react";
+import { createStore } from "redux";
+
+import { renderWrapper } from "../../utils/testSetup";
+
+import Login from "./Login";
+
+afterEach(cleanup);
+
+describe("<Login />", () => {
+  test("renders the Login component", () => {
+    const { container } = renderWrapper(<Login />);
+    const dataTestId = container.getElementsByTagName("div")[0];
+    expect(dataTestId.getAttribute("data-testid")).toBe("login-component");
+  });
+
+  test("renders <LoginScreen /> when userToken has setted", () => {
+    const store = createStore(() => ({ userToken: "usertoken" }));
+    const { container } = renderWrapper(<Login />, store);
+    const dataTestId = container.querySelectorAll("div")[1];
+    expect(dataTestId.getAttribute("data-testid")).toBe("login-screen");
+  });
+});
